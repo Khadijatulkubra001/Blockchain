4f9e3b44df34dc1d4da813c8b72959fa41a8f399
Khadija <i200760@nu.edu.pk>
2023-10-07 20:27:13 +0500

    Assignment done

---
+++ assignment01bca/assignment01bca.go
@@ -0,0 +1,97 @@
+package assignment01bca
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
+)
+
+// Block represents a single block in the blockchain.
+type Block struct {
+	Transaction  string
+	Nonce        int
+	PreviousHash string
+	CurrentHash  string
+}
+
+// NewBlock adds a new block to the blockchain with the given transaction, nonce, and previous hash.
+func NewBlock(transaction string, nonce int, previousHash string) *Block {
+	block := &Block{
+		Transaction:  transaction,
+		Nonce:        nonce,
+		PreviousHash: previousHash,
+	}
+	block.CalculateHash()
+	return block
+}
+
+// calculateHash calculates and sets the hash of the block based on its data.
+func (b *Block) CalculateHash() {
+	data := fmt.Sprintf("%s%d%s", b.Transaction, b.Nonce, b.PreviousHash)
+	hashBytes := sha256.Sum256([]byte(data))
+	b.CurrentHash = hex.EncodeToString(hashBytes[:])
+}
+
+// DisplayBlocks prints all the blocks in the blockchain in a nice format.
+func DisplayBlocks(blocks []*Block) {
+	fmt.Println("Blockchain:")
+	for _, block := range blocks {
+		block.displayBlockInfo()
+	}
+}
+
+// ChangeBlockTransaction updates the transaction of a given block.
+func ChangeBlock(block *Block, newTransaction string) {
+
+	// Save the original hash for later comparison
+	originalHash := block.CurrentHash
+
+	block.Transaction = newTransaction
+	block.CalculateHash() // Recalculate the hash after changing the transaction
+
+	// Check if the recalculated hash is different from the original hash
+	if block.CurrentHash != originalHash {
+		fmt.Printf("Error: Invalid transaction in Block: %s\n", newTransaction)
+	}
+}
+
+// VerifyChain verifies the integrity of the blockchain in case any changes are made.
+func VerifyChain(blocks []*Block) bool {
+	for i := 1; i < len(blocks); i++ {
+		currentBlock := blocks[i]
+		previousBlock := blocks[i-1]
+
+		// Verify hash
+		if currentBlock.CurrentHash != currentBlock.calculateHashValue() {
+			return false
+		}
+
+		// Verify previous hash reference
+		if currentBlock.PreviousHash != previousBlock.CurrentHash {
+			return false
+		}
+	}
+	return true
+}
+
+// CalculateHash calculates the hash of a given string.
+func CalculateHash(stringToHash string) string {
+	hashBytes := sha256.Sum256([]byte(stringToHash))
+	return hex.EncodeToString(hashBytes[:])
+}
+
+// calculateHashValue calculates and returns the hash of the block's data.
+func (b *Block) calculateHashValue() string {
+	data := fmt.Sprintf("%s%d%s", b.Transaction, b.Nonce, b.PreviousHash)
+	hashBytes := sha256.Sum256([]byte(data))
+	return hex.EncodeToString(hashBytes[:])
+}
+
+// displayBlockInfo prints the block's information.
+func (b *Block) displayBlockInfo() {
+	fmt.Println("Transaction:", b.Transaction)
+	fmt.Println("Nonce:", b.Nonce)
+	fmt.Println("Previous Hash:", b.PreviousHash)
+	fmt.Println("Current Hash:", b.CurrentHash)
+	fmt.Println("--------------------------")
+}
---
+++ main.go
@@ -0,0 +1,56 @@
+// main.go
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+
+	"blockchain/assignment01bca"
+)
+
+func main() {
+	// Create a blockchain and add some blocks
+	blockchain := []*assignment01bca.Block{}
+	genesisBlock := assignment01bca.NewBlock("Genesis Transaction", 0, "")
+	blockchain = append(blockchain, genesisBlock)
+
+	// Add more blocks with random nonces
+	rand.Seed(time.Now().UnixNano()) // Seed the random number generator
+
+	block1 := assignment01bca.NewBlock("Alice to Bob", rand.Int(), genesisBlock.CurrentHash)
+	blockchain = append(blockchain, block1)
+
+	block2 := assignment01bca.NewBlock("Bob to Carol", rand.Int(), block1.CurrentHash)
+	blockchain = append(blockchain, block2)
+
+	block3 := assignment01bca.NewBlock("Carol to Susan", rand.Int(), block2.CurrentHash)
+	blockchain = append(blockchain, block3)
+
+	block4 := assignment01bca.NewBlock("Susan to Mark", rand.Int(), block3.CurrentHash)
+	blockchain = append(blockchain, block4)
+
+	block5 := assignment01bca.NewBlock("Mark to Alice", rand.Int(), block4.CurrentHash)
+	blockchain = append(blockchain, block5)
+
+	block6 := assignment01bca.NewBlock("Hello, I am Khadija", rand.Int(), block5.CurrentHash)
+	blockchain = append(blockchain, block6)
+
+	// Display all blocks in the blockchain
+	assignment01bca.DisplayBlocks(blockchain)
+
+	// Attempt to modify a block's transaction (Simulating an invalid blockchain)
+	 /*modifiedBlockIndex := 2
+	modifiedTransaction := "Modified Transaction"
+	assignment01bca.ChangeBlock(blockchain[modifiedBlockIndex], modifiedTransaction)
+	fmt.Printf("Modifications made at Block %d: The modification made was: %s\n", modifiedBlockIndex, modifiedTransaction)
+	*/
+
+	// Verify the blockchain's integrity
+	isValid := assignment01bca.VerifyChain(blockchain)
+	if isValid {
+		fmt.Println("Blockchain is valid.")
+	} else {
+		fmt.Println("Blockchain is not valid.")
+	}
+}
